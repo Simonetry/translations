c5a980e687449ec805799d498f4ad00a7fe7007c
Feeling sillay <everyday@live.se>
2026-04-26 23:25:52 +0200

    Completed

---
+++ sv/front.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'current' => 'Frontar just nu',
+    'none' => 'Ingen frontar för tillfället.',
+    'started' => 'Front påbörjad.',
+    'history' => 'Fronthistorik',
+    'comment' => 'Kommentar',
+    'co_fronting' => 'Co-fronting',
+    'co_con' => 'Co-con',
+    'duration' => 'Varaktighet',
+    'started_at' => 'Startade kl.',
+    'ended_at' => 'Slutade kl.',
+    'started_fronting' => 'frontar nu.',
+    'stopped_fronting' => 'har lämnat front.',
+    'changed_front_type' => 'ändrades från :old till :new.',
+];
