cb87d6cd8d703e07b5a621706cc9159293e79906
Feeling sillay <everyday@live.se>
2026-04-26 23:28:28 +0200

    Completed

---
+++ sv/journal.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Titel',
+    'content' => 'Innehåll',
+    'private' => 'Privat',
+    'shared' => 'Delad',
+    'created' => 'Journalinlägg skapat.',
+    'updated' => 'Journalinlägg uppdaterat.',
+    'deleted' => 'Journalinlägg borttaget.',
+    'encrypted_notice' => 'Journalinlägg är krypterade och kan endast läsas av ditt system.',
+];
