3434648955f0358c2495072810a024a025c08927
Feeling sillay <everyday@live.se>
2026-04-26 23:32:47 +0200

    Completed

---
+++ sv/member.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'name' => 'Medlemsnamn',
+    'display_name' => 'Visningsnamn',
+    'pronouns' => 'Pronomen',
+    'description' => 'Beskrivning',
+    'role' => 'Roll',
+    'color' => 'Färg',
+    'created' => 'Medlem skapad.',
+    'updated' => 'Medlem uppdaterad.',
+    'deleted' => 'Medlem borttagen.',
+    'archived' => 'Medlem arkiverad.',
+    'unarchived' => 'Medlem återställd från arkiv.',
+    'is_custom_front' => 'Anpassad front',
+    'is_custom_front_description' => 'Ett namngivet front-läge som inte är bundet till en specifik alter.',
+];
