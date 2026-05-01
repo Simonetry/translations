d05269906c906533f1916d4021617b0336ddf1c4
Drexel <drexelquinn@gmail.com>
2026-04-30 18:53:02 -0700

    fix: Adding the missing title

--- en/kb.php
+++ en/kb.php
@@ -14,20 +14,15 @@ return [
     'featured_articles' => 'Featured Articles',
     'no_articles_in_category' => 'No articles in this category yet.',
     'browse_category_articles' => 'Browse :category articles in the PluralSpace Help Center.',
-
-    // Article page
     'last_updated' => 'Last updated :date',
     'tags_label' => 'Tags:',
     'related_articles' => 'Related articles',
     'read_in_help_center' => 'Read :title in the PluralSpace Help Center.',
-
-    // Article vote
     'was_this_helpful' => 'Was this helpful?',
     'like_aria' => 'Like this article',
     'dislike_aria' => 'Mark this article as not helpful',
     'dislike_title' => 'Not helpful',
-
-    // Footer
+    'title' => 'Help Center',
     'tagline' => 'A cozy, safe home for your system.',
     'footer_discord' => 'Discord',
 ];
